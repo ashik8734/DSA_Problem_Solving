a8b26fbe243a940e13413013d790e480aa1c3064
Ashik Shaikh <121289788+ashik8734@users.noreply.github.com>
2024-02-06 19:17:32 +0530

    Add files via upload

---
+++ Arrays/SubArraySum.java
@@ -0,0 +1,24 @@
+class SubArraySum {
+    public static int subarraySum(int[] nums, int k) {
+        //Better Approach TC-O(n*n)
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            for(int j=i;j<nums.length;j++){
+             sum=sum+nums[j];
+             if(sum==k){
+                 count++;
+             }
+            }
+        }
+    return count;    
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4};
+        int k = 6;
+        int cnt = subarraySum(arr, k);
+        System.out.println("The number of subarrays is: " + cnt);
+
+    }
+}
