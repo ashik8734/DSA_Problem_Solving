d1743e72161f70b5dd91162461f4ca3629b74977
Ashikali Shaikh <121289788+ashik8734@users.noreply.github.com>
2024-02-09 12:41:13 +0530

    Add files via upload

---
+++ Arrays/SortColor.java
@@ -0,0 +1,30 @@
+public class SortColor {
+    public static void sortColors(int[] nums) {
+        int zero=0, one=0, two=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0) zero++;
+            if(nums[i]==1) one++;
+            if(nums[i]==2) two++;
+        }
+
+        for(int i=0;i<zero;i++){
+            nums[i]=0;
+        }
+        for(int i=zero;i<zero+one;i++){
+            nums[i]=1;
+        }
+        for(int i=zero+one;i<zero+one+two;i++){
+            nums[i]=2;
+        }
+
+        for(int i =0 ;i < nums.length; i++ ){
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {0, 2, 1, 2, 0, 1};
+        sortColors(nums);
+    }
+}
